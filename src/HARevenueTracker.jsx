00d4759b23b1d70056b677cffa8dd305ae04414a
Shanthosh Krishnakumar <shanthosh@hainesattract.co.nz>
2021-10-09 22:51:57 +1300

    HARevenueTrackerGroup: Copy the same content from BulletGroup

--- src/HARevenueTracker.jsx
+++ src/HARevenueTracker.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import HARevenueTrackerGroup from './charts/HARevenueTrackerGroup';
+import './dashboard.css';
+
+const haRevenueTrackerData = require('./data/haRevenueTrackerData.json');
 
 const HARevenueTracker = () => {
-  return <div>Revenue Tracker</div>;
+  return (
+    <div className="dashboard">
+      {haRevenueTrackerData.map((group, index) => {
+        const vertical = group.vertical || false,
+          themeName = group.theme || null,
+          title = group.title || null,
+          charts = group.charts,
+          cls = group.class;
+        return (
+          <HARevenueTrackerGroup
+            title={title}
+            vertical={vertical}
+            charts={charts}
+            themeName={themeName}
+            class={cls}
+            key={index}
+          ></HARevenueTrackerGroup>
+        );
+      })}
+    </div>
+  );
 };
 
 export default HARevenueTracker;
