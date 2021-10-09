73a1a7e87ac22840f5f0f0d5d274e71143502d77
Shanthosh Krishnakumar <shanthosh@hainesattract.co.nz>
2021-10-09 20:05:29 +1300

    minor Prettier formatting changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './dashboard.css';
 import './App.css';
 import BulletGroup from './charts/BulletGroup';
 var groups = require('./data/bullets.json');
 
-class App extends Component {
-    render() {
-        return(
-            <div className="dashboard">
-                {
-                    groups.map((group, index) => {
-                        const   vertical = group.vertical || false,
-                                themeName = group.theme || null,
-                                title = group.title || null,
-                                charts = group.charts,
-                                cls = group.class;
-                        return  (
-                            <BulletGroup
-                                title={title}
-                                vertical={vertical}
-                                charts={charts}
-                                themeName={themeName}
-                                class={cls}
-                                key={index}
-                            ></BulletGroup>
-                        );
-                    })
-                }
-            </div>
-        )
-    }
-}
+const App = () => {
+  return (
+    <div className="dashboard">
+      {groups.map((group, index) => {
+        const vertical = group.vertical || false,
+          themeName = group.theme || null,
+          title = group.title || null,
+          charts = group.charts,
+          cls = group.class;
+        return (
+          <BulletGroup
+            title={title}
+            vertical={vertical}
+            charts={charts}
+            themeName={themeName}
+            class={cls}
+            key={index}
+          ></BulletGroup>
+        );
+      })}
+    </div>
+  );
+};
 
-export default App
+export default App;
